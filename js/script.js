6aa7447e64e19a95916dec908530bf376c3e8d04
wannit9 <espoir980921@gmail.com>
2021-12-17 16:43:57 +0900

    21.12.17 commit
    edit css, html
    tried using scss

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(function(){
     $(".fadeinout li").hide();
-    // $(".fadeinout li").first().fadeIn();
-    $(".cakeimg ul").click(function(){
+    $(".fadeinout li").first().fadeIn();
+    $(".cakeimg li").click(function(){
         var idx = $(this).index();
-        $(".fadeinout").stop().eq(idx).fadeIn();
+        $(".fadeinout").eq(idx).fadeIn();
     });
 });
